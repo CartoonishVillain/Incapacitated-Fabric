e4bbfba2013008c86e2c07dab0687f01c099fcba
Cartoonish Villain <jedijoe93@gmail.com>
2021-11-14 17:52:46 -0500

    Configs readded

--- src/main/java/com/cartoonishvillain/incapacitated/Incapacitated.java
+++ src/main/java/com/cartoonishvillain/incapacitated/Incapacitated.java
@@ -1,14 +1,25 @@
 package com.cartoonishvillain.incapacitated;
 
+import com.cartoonishvillain.incapacitated.config.IncapacitationConfig;
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.ResourceLocationException;
+import net.minecraft.resources.ResourceLocation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Incapacitated implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LogManager.getLogger("incapacitated");
+	public static IncapacitationConfig config;
+	public static ArrayList<ResourceLocation> ReviveFoods;
+	public static ArrayList<ResourceLocation> HealingFoods;
 
 	@Override
 	public void onInitialize() {
@@ -16,5 +27,43 @@ public class Incapacitated implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
+		AutoConfig.register(IncapacitationConfig.class, GsonConfigSerializer::new);
+		config = AutoConfig.getConfigHolder(IncapacitationConfig.class).getConfig();
+
+		ReviveFoods = getFoodForReviving();
+		HealingFoods = getFoodForHealing();
+
+	}
+
+	private ArrayList<ResourceLocation> getFoodForReviving() {
+		final String FoodList = config.config.REVIVEFOODS;
+		String[] reviveFoods = FoodList.split(",");
+		ArrayList<ResourceLocation> reviveFoodList = new ArrayList<>();
+		try {
+			for(String string : reviveFoods){
+				ResourceLocation food = new ResourceLocation(string);
+				reviveFoodList.add(food);
+			}
+		}catch(ResourceLocationException e){
+			Incapacitated.LOGGER.error("Incapacitation: Revive foods not parsed. Non [a-z0-9_.-] character in config! Using default...");
+			return new ArrayList<>(List.of(new ResourceLocation("enchanted_golden_apple")));
+		}
+		return reviveFoodList;
+	}
+
+	private ArrayList<ResourceLocation> getFoodForHealing() {
+		final String FoodList = config.config.HEALINGFOODS;
+		String[] healFoods = FoodList.split(",");
+		ArrayList<ResourceLocation> healFoodList = new ArrayList<>();
+		try {
+			for(String string : healFoods){
+				ResourceLocation food = new ResourceLocation(string);
+				healFoodList.add(food);
+			}
+		}catch(ResourceLocationException e){
+			Incapacitated.LOGGER.error("Incapacitation: Healing foods not parsed. Non [a-z0-9_.-] character in config! Using default...");
+			return new ArrayList<>(List.of(new ResourceLocation("golden_apple")));
+		}
+		return healFoodList;
 	}
 }
---
+++ src/main/java/com/cartoonishvillain/incapacitated/config/IncapacitationConfig.java
@@ -0,0 +1,30 @@
+package com.cartoonishvillain.incapacitated.config;
+
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
+import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
+
+@Config(name = "incapacitation")
+public class IncapacitationConfig implements ConfigData {
+
+    @ConfigEntry.Gui.CollapsibleObject
+    public IncapacitationOptions config = new IncapacitationOptions();
+
+    public static class IncapacitationOptions{
+        @Comment("A list of comma separated item IDs for foods player can eat to revive themselves. IE: minecraft:enchanted_golden_apple,minecraft:apple")
+        public String REVIVEFOODS = "minecraft:enchanted_golden_apple";
+        @Comment("A list of comma separated item IDs for foods player can eat to reset their down counters. IE: minecraft:golden_apple,minecraft:golden_carrot")
+        public String HEALINGFOODS = "minecraft:golden_apple";
+        @Comment("How many ticks a player can be downed without dying")
+        public int DOWNTICKS = 2000;
+        @Comment("How long it takes to revive a downed player manually")
+        public int REVIVETICKS = 150;
+        @Comment("How many times a player can go down without a healing or revive item, without instantly dying the next time they are supposed to go down.")
+        public int DOWNCOUNT = 3;
+        @Comment("Do players glow while downed to be easier to find?")
+        public boolean GLOWING = true;
+        @Comment("Are downed players invincible?")
+        public boolean INVINCIBLEDOWN = false;
+    }
+}
